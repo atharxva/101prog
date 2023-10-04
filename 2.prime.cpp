dc4d2dc3387e7c11508855d5628cbdc95763bd51
atharxva <142775300+atharxva@users.noreply.github.com>
2023-10-04 10:03:59 +0530

    2.prime.cpp

--- 2.prime.cpp
+++ 2.prime.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 using namespace std;
 
@@ -8,12 +6,23 @@ int main() {
     cout << "Enter n: ";
     cin >> n;
 
-    if (n == 0)
-        cout << "This is prime"; // Comment: Checking if n is equal to 0
-
-    else if (n < 0) {
-        cout << "This is not prime"; // Comment: Handling negative numbers
+    if (n <= 1) {
+        cout << "This is not prime"; // 0 and 1 are not prime
     } else {
-        cout << "This is prime"; // Comment: Assuming all other positive integers are prime
+        bool isPrime = true;
+        for (int i = 2; i * i <= n; ++i) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            cout << "This is prime"; // n is prime
+        } else {
+            cout << "This is not prime"; // n is not prime
+        }
     }
+
+    return 0;
 }
